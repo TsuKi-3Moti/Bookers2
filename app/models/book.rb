11857eb1f49d8313a90c74917237f6ac783b370a
AyaneSuto <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-04-16 06:49:51 +0000

    [Add] BookとUserのアソシエーション

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,20 +1,4 @@
 class Book < ApplicationRecord
   has_one_attached :image
-
-  def new
-    @book = Book.new
-  end
-
-  def create
-    @book = Book.find(params[:id])
-    @book.save
-    redirect_to books_path
-  end
-
-  def index
-  end
-
-  def show
-  end
-
+  belongs_to :user
 end
