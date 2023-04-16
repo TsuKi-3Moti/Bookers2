b997624ab9a80a74d4105b2c3334f35b3a373561
AyaneSuto <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-04-16 04:56:05 +0000

    [Add] usersコントローラの作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,20 @@
 class Book < ApplicationRecord
   has_one_attached :image
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.find(params[:id])
+    @book.save
+    redirect_to books_path
+  end
+
+  def index
+  end
+
+  def show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :books
   devise_for :users
   root to: "homes#top"
+  resources :books
+  resources :users, only: [:show, :index, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
