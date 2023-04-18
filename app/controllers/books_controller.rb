8b51090cceaa7ec3dbafce9be6dccaa5f6fc110c
AyaneSuto <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-04-18 09:03:25 +0000

    [Add] User infoの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,25 @@
 class BooksController < ApplicationController
-  def new
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
+    @book = Book.new
+    @book.user = current_user
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,37 @@
 class UsersController < ApplicationController
-  def show
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to user_path(@book.id)
   end
 
   def index
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books #特定のユーザ（@user）に関連付けられた投稿全て（books）を取得し@booksに格納
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:profile_image, :name, :introduction)
+  end
+
 end
